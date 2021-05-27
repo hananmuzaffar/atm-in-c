ee46d48f2e87c07d411d9775d789ef24ffab61f9
Hanan Muzaffar <33029514+hananmuzaffar@users.noreply.github.com>
2021-05-27 09:19:22 +0530

    Create password-generator.c

---
+++ password-generator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	int i,password,len;
+	srand((unsigned int)time(NULL));
+	printf("Enter Password length and must be greater than 8: ");
+	scanf("%d",&len);
+	if(len>=8)
+	{
+		printf("Generated Password is: ");
+		for(i=0;i<len;i++)
+		{
+			int k=rand()%128;
+			if((k>48&&k<=57)||(k>64&&k<=90)||(k>=97&&k<=122)||(k>35&&k<=37)||k==64)
+			{
+				printf("%c",k);
+			}
+			else
+			{
+				i--;
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		printf("Length should be greater than 8");
+	}
+return 0;
+}
